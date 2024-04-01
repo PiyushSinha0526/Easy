29390d3e7f394acd6a401ee28c8c4f53df139323
Piyush Sinha <piyushsinha0526@gmail.com>
2024-04-01 11:28:32 +0530

    feat: zod common module

---
+++ common/src/index.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+export const signupInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().min(3).optional(),
+});
+
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const createBlogIbput = z.object({
+  title: z.string(),
+  content: z.string(),
+  published: z.date(),
+});
+
+export const updateBlogIbput = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.number(),
+});
+
+// type inference in zod
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signupInput>;
+export type CreateBlogIbput = z.infer<typeof createBlogIbput>;
+export type UpdateBlogIbput = z.infer<typeof updateBlogIbput>;
