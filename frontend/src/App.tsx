29576cac2ec9eccc6ae46565c30032800152d456
Piyush Sinha <piyushsinha0526@gmail.com>
2024-04-05 00:05:19 +0530

    add: routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Quote from './components/Quote'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <>
-    <div className='grid grid-cols-2'>
-      <div></div>
-      <Quote/>
-    </div>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/blog/:id" element={<Blog/>}/>
+        </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
