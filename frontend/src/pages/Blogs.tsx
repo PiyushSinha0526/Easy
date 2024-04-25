f202377392958cd2c01647de9f5fd384ca9384f6
Piyush Sinha <piyushsinha0526@gmail.com>
2024-04-26 01:27:03 +0530

    remove: unused code

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,13 +1,6 @@
 import BlogRow from "../components/BlogRow";
 import {useBlogs} from "../hooks";
-const mockData = {
-  authorName: "Lorem",
-  title:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, sint.",
-  content:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consectetur obcaecati vero iure at doloremque fuga! Eius provident praesentium excepturi voluptas accusantium quae sed, debitis, animi at, repellat alias hic asperiores maxime!",
-  published: "10-10-10",
-};
+
 const Blogs = () => {
   const { loading, blogs } = useBlogs();
   if (loading) return <div>Loading...</div>;
