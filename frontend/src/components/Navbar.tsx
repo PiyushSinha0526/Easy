da93ab8f17150735d740f35b8967ff58356de0d6
Piyush Sinha <piyushsinha0526@gmail.com>
2024-05-19 22:17:56 +0530

    added: logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
-
+import logo from '../assets/logo.png';
 function Navbar() {
   return (
     <>
-      <div className="flex justify-between border-b-2 px-8 py-3">
-        <span>logo</span>
-        <ul className="flex gap-2">
-          <li>1</li>
-          <li>2</li>
-          <li>
-            <Link to={'/publish'}
-              type="button"
-              className="mb-2 me-2 rounded-lg bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br"
-            >
-              Publish
-            </Link>
-          </li>
-        </ul>
-        <span>0</span>
+      <div className="flex justify-between items-center border-b-2 px-8">
+        <div><img src={logo} alt="logo" className="w-14"/></div>
+        <div className="flex gap-4">
+          <ul className="flex gap-2">
+            <li>1</li>
+            <li>2</li>
+            <li>
+              <Link to={'/publish'}
+                type="button"
+                className="mb-2 me-2 rounded-lg bg-gradient-to-r from-green-400 via-green-500 to-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br"
+              >
+                Publish
+              </Link>
+            </li>
+          </ul>
+          <span>0</span>
+        </div>
       </div>
       <Outlet />
     </>
