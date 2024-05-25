f892d6410c55ec910259608eb100ae12caf6df11
Piyush Sinha <piyushsinha0526@gmail.com>
2024-05-26 02:08:28 +0530

    added: tiptap Editor starter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Blog from "./pages/Blog";
 import Blogs from "./pages/Blogs";
 import Navbar from "./components/Navbar";
 import Publish from "./pages/Publish";
+import Editor from "./components/Editor";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/signin" element={<Signin/>}/>
           <Route path="" element={<Navbar />}>
+            <Route path="/" element={<Editor/>}/>
             <Route path="blog/:id" element={<Blog/>}/>
             <Route path="blogs" element={<Blogs/>}/>
             <Route path="/publish" element={<Publish/>}/>
