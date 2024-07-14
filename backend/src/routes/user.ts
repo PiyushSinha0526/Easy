f8f1c9fded4966ffa73a27cda18d604f886fd8f4
Piyush Sinha <piyushsinha0526@gmail.com>
2024-07-14 16:09:10 +0530

    forward userId

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -32,7 +32,7 @@ userRouter.post("/signup", async (c) => {
       },
     });
     const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ email: user.email, name: user.name, jwt: token });
+    return c.json({ email: user.email, name: user.name, id: user.id, jwt: token });
   } catch (error) {
     c.status(411);
     c.text("Invalid");
@@ -63,7 +63,7 @@ userRouter.post("/signin", async (c) => {
       return c.json({ error: "user not found" });
     }
     const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ email: user.email, name: user.name, jwt: token });
+    return c.json({ email: user.email, name: user.name, id: user.id, jwt: token });
   } catch (error) {
     c.status(411);
     c.text("Invalid");
